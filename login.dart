71b8690d5497e5bb45608484f650d6b46e57a319
Zakarya Muhammad <77733277+zakarya0@users.noreply.github.com>
2022-08-01 12:45:51 +0300

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fpl/screens/register.dart';
+import 'register.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
